56a0193e8e342f21e99a6382fd050293a396dbf6
marie-claude lam <marieclaude.lam@gmail.com>
2019-11-08 11:21:31 +0100

    js : canvas : création du cadre

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+    var canvas = document.createElement('canvas');
+    canvas.width = 900;
+    canvas.height = 600;
+    canvas.style.border ="1px solid";
+    document.body.appendChild(canvas);
+
+}
