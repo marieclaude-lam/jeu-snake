79b6fc8de41521605a9ea67ed897a7e914a61a3c
marie-claude lam <marieclaude.lam@gmail.com>
2019-11-08 12:05:51 +0100

    js: j'anime le serpent

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 window.onload = function()
 {
-//dessin du cadre
-    var canvas = document.createElement('canvas');
-    canvas.width = 900;
-    canvas.height = 600;
-    canvas.style.border ="1px solid";
-    document.body.appendChild(canvas);
+    var canvas;
+    var ctx;
+    var delay = 100;//en ms.
+    var xCoord = 0;
+    var yCoord = 0;
 
-//dessin du rectangle
-    var ctx = canvas.getContext('2d');
+    init();
+
+    function init()
+        {
+            //dessin du cadre
+                canvas = document.createElement('canvas');
+                canvas.width = 900;
+                canvas.height = 600;
+                canvas.style.border ="1px solid";
+                document.body.appendChild(canvas);
+                ctx = canvas.getContext('2d');
+                refreshCanvas();
+        }
+
+    function refreshCanvas()
+        {
+//
+            xCoord += 2;
+            yCoord += 2;
+// le snake va se déplacer, il faut donc effacer une partie.
+            ctx.clearRect(0,0,canvas.width, canvas.height);
 //couleur rouge
-    ctx.fillStyle = '#ff0000';
+            ctx.fillStyle = '#ff0000';
 // dessin rect ( x (a partir du coin haut gauche), y (à partir coin haut gauche), largeur du rect, hauteur du rect)
-    ctx.fillRect(30 , 30, 100, 50);
-
+            ctx.fillRect(xCoord,yCoord,100,50);//on va mettre à jour le x et le y
+            setTimeout(refreshCanvas,delay);
+    }
 }
